aea8442c43dc5797e71dffdf6e3d99b458a2af41
Suman Bhandari <sumanbhandari9860@gmail.com>
2023-07-08 23:50:19 +0530

    Added add cart feature

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,16 @@
+import './cart-item.styles.scss'
+
+const CartItem = ({cartitem}) => {
+    const { name,imageUrl,price ,quantity } = cartitem;
+   return (
+        <div className='cart-item-container'>
+           <img src={imageUrl} alt={`${name}`} />
+           <div className='item-details'>
+               <span className='name'>{name }</span>   
+               <span>{quantity}X${ price}</span>
+           </div>
+        </div>
+    )
+}
+
+export default CartItem;
