fa6f23c4e2349d800f7ef151c352cb3e642ae843
Suman Bhandari <sumanbhandari9860@gmail.com>
2023-07-10 22:51:09 +0530

    Increment Decrement and total price functionality added

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -12,6 +12,17 @@ const addCartItem = (cartItems, productToAdd) => {
     
 }
 
+const decrementCartQuantity =  (cartItems,productTodecrement)=>{
+    const existitem = cartItems.find((cartItem) => cartItem.id === productTodecrement.id)
+    
+    if (existitem) {
+        return cartItems.map((cartItem) => {
+            return cartItem.id === productTodecrement.id ? { ...cartItem, quantity:cartItem.quantity!==0?cartItem.quantity-1:cartItem.quantity=0 } : cartItem;
+            
+        })
+    }
+}
+
 
 
 export const CartContext = createContext({
@@ -26,7 +37,7 @@ export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [cartCount, setCartCount] = useState(0);
-     
+    const [total, setTotal] = useState(0);
     useEffect(()=>{
         const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
         setCartCount(newCartCount)
@@ -37,6 +48,31 @@ export const CartProvider = ({ children }) => {
         setCartItems(addCartItem(cartItems, productToAdd));
     }
 
-    const value = {isCartOpen,setIsCartOpen,addItemToCart,cartItems,cartCount}
+    const decrementQuantity = (productTodecrement) => {
+        setCartItems(decrementCartQuantity(cartItems,productTodecrement))
+    }
+
+    const totalvalue = () => {
+        const val =  cartItems.map((cartItem) => {
+            return {...cartItem,price:cartItem.price*cartItem.quantity}
+        })
+       
+       return val.reduce((sum, cartItem) => cartItem.price+sum,0)
+        
+    }
+
+    useEffect(() => {
+        setTotal(totalvalue);
+    }, [totalvalue])
+    
+
+    // const removeItem = (item) => {
+    //     return cartItems.map((cartItem) => {
+    //         return car
+    //     })
+    // }
+
+    const value = {isCartOpen,setIsCartOpen,addItemToCart,cartItems,cartCount,decrementQuantity,total}
     return <CartContext.Provider value={value}>{ children}</CartContext.Provider>
 }
+
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,22 +3,28 @@ import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
 import { ProductsContext } from "../../contexts/products.context";
 const CheckOut = () => {
-    const { addItemToCart, cartItems } = useContext(CartContext);
-    const { products } = useContext(ProductsContext);
-    return <div>
+    const { addItemToCart, cartItems,decrementQuantity,total, } = useContext(CartContext);
+    
+    return(
+    <>
+    <div style={{display:"flex"}}>
         {cartItems.map((cartItem) => {
-            return <div>
+            return <div key={cartItem.id}>
                 <h1>{cartItem.name} and </h1>
                 <img src={cartItem.imageUrl} alt="Image" />
-                <span>Quantity {cartItem.quantity}</span>
+                <button>X</button>
                 <h1 onClick={()=>addItemToCart(cartItem)} >+</h1>
-                <h1 onClick={()=>addItemToCart(cartItem)} >-</h1>
+                <h1 onClick={()=>decrementQuantity(cartItem)} >-</h1>
+                <span>Quantity {cartItem.quantity}X${cartItem.price}</span>
                 </div>
                              
         })}
          
-            
-    </div>
+          
+            </div>
+            <h1>TOTAL:{total }</h1>
+        </>
+    )
     
 }
 
