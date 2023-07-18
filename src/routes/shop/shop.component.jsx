5ce49bda29f2f6350579519ddc3b3652a65f6644
Suman Bhandari <sumanbhandari9860@gmail.com>
2023-07-18 22:32:41 +0530

    nested routes for particular product

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,36 +1,16 @@
-import { useContext ,Fragment} from 'react';
-import { CategoriesContext } from '../../contexts/categories.context';
-import ProductCard from '../../components/product-card/product-card.component';
+import { Routes,Route } from 'react-router-dom';
+import CategoriesPreview from '../categories-preview.component/categories-preview.component';
 import './shop.styles.scss'
-
+import Category from '../category/category.component';
 
 const Shop = () => {
-    const { categoriesMap } = useContext(CategoriesContext);
-    return (
-        <>
-            
-                {
-                    Object.keys(categoriesMap).map((title) => {
-                      return(  < Fragment key={title}>
-                            <h2>{title }</h2>
-                             <div className='products-container'>
-            
-                               { categoriesMap[title].map((product) => {
-                                    return (
-                                        <ProductCard key={product.id} product={product}/>
-                                    )
-                                })
-                                }
-                            </div>
-                        </Fragment>
-                      )
-                    })
-                }
-            
-           
+return (
+    <Routes>
+        <Route index element={<CategoriesPreview />} />
+        <Route path=":category" element = {<Category/>}/>
         
-        </>
+    </Routes>
     )
-} 
+}; 
 
 export default Shop;
